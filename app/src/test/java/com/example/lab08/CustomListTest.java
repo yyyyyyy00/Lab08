be79ef00f62c9d3e069d8c526f27867735d2d14f
yyyyyyy00 <winter77211@outlook.com>
2026-03-04 18:48:21 -0700

    pass all test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -11,13 +11,26 @@ public class CustomListTest {
         // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
     }
+    @Test void testAddCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        assertTrue(list.hasCity(calgary));
+    }
     @Test
     public void testDeleteCity() {
         CustomList list = new CustomList();
         City city = new City("Edmonton", "AB");
-        list.addCity(city);
         list.deleteCity(city);
-        list.countCities();
         assertFalse(list.hasCity(city));
+    }
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(calgary);
+        list.addCity(edmonton);
+        assertEquals(2, list.countCities());
     }
 }
